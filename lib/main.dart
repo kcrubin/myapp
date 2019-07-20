14607a7b0442c8cfc2ecc0d7dd6decf4d185f8d6
kcrubin123@gmail.com <kcrubin123@gmail.com>
2019-07-20 22:37:23 +0545

    temp

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 void main() {
   //create a new text widget to show on screen
   var app = MaterialApp( //instance of dart class; its MaterialApp widget
-    home: Text('Hi Dear'),
+    home: Text('Hi There'),
     );
 //take that widget and get it on screen
 runApp(app);
